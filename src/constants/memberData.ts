8048825700c46e1d6aa94e5214e52416bd35eafc
jungyungee <jungyungee0610@gmail.com>
2025-12-19 21:34:02 +0900

    [CHORE] 백엔드 팀 변경 및 멤버 오름차순 정렬

--- src/constants/memberData.ts
+++ src/constants/memberData.ts
@@ -1,7 +1,7 @@
 import type { Member } from '@/types/member';
 
-// 프론트엔드 멤버 목록
-export const frontendMembers: Member[] = [
+// 프론트엔드 멤버 목록 (이름 오름차순)
+export const frontendMembers: Member[] = ([
   { id: 'fe-1', name: '정윤지', team: 'MODELLY', university: 'HONGIK UNIV' },
   { id: 'fe-2', name: '손주완', team: 'MODELLY', university: 'HONGIK UNIV' },
   { id: 'fe-3', name: '이채연', team: 'STORIX', university: 'HONGIK UNIV' },
@@ -12,18 +12,18 @@ export const frontendMembers: Member[] = [
   { id: 'fe-8', name: '신용섭', team: 'MENUAL', university: 'SEOGANG UNIV' },
   { id: 'fe-9', name: '정성훈', team: 'CATCHUP', university: 'HONGIK UNIV' },
   { id: 'fe-10', name: '최무헌', team: 'MENUAL', university: 'HONGIK UNIV' },
-];
+] as Member[]).sort((a, b) => a.name.localeCompare(b.name, 'ko'));
 
-// 백엔드 멤버 목록
-export const backendMembers: Member[] = [
+// 백엔드 멤버 목록 (이름 오름차순)
+export const backendMembers: Member[] = ([
   { id: 'be-1', name: '변호영', team: 'DIGGINDIE', university: 'YONSEI UNIV' },
-  { id: 'be-2', name: '이윤지', team: 'STORIX', university: 'EWHA UNIV' },
+  { id: 'be-2', name: '이윤지', team: 'DIGGINDIE', university: 'EWHA UNIV' },
   { id: 'be-3', name: '이연호', team: 'MODELLY', university: 'HONGIK UNIV' },
   { id: 'be-4', name: '이준영', team: 'MODELLY', university: 'YONSEI UNIV' },
-  { id: 'be-5', name: '배승식', team: 'MENUAL', university: 'SEOGANG UNIV' },
+  { id: 'be-5', name: '배승식', team: 'CATCHUP', university: 'SEOGANG UNIV' },
   { id: 'be-6', name: '신혁', team: 'CATCHUP', university: 'HONGIK UNIV' },
-  { id: 'be-7', name: '이지원', team: 'STORIX', university: 'HONGIK UNIV' },
-  { id: 'be-8', name: '변하영', team: 'CATCHUP', university: 'EWHA UNIV' },
-  { id: 'be-9', name: '서가영', team: 'DIGGINDIE', university: 'HONGIK UNIV' },
-  { id: 'be-10', name: '이수아', team: 'MENUAL', university: 'SEOGANG UNIV' },
-];
+  { id: 'be-7', name: '이지원', team: 'MENUAL', university: 'HONGIK UNIV' },
+  { id: 'be-8', name: '변하영', team: 'MENUAL', university: 'EWHA UNIV' },
+  { id: 'be-9', name: '서가영', team: 'STORIX', university: 'HONGIK UNIV' },
+  { id: 'be-10', name: '이수아', team: 'STORIX', university: 'SEOGANG UNIV' },
+] as Member[]).sort((a, b) => a.name.localeCompare(b.name, 'ko'));
